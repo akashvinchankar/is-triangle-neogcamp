b6fc81ce3ca8644f2409e3461d70f2cae8f534ce
akashvinchankar <86399324+akashvinchankar@users.noreply.github.com>
2021-12-06 18:36:40 +0530

    hypotenuse html js

---
+++ hypotenuse.js
@@ -0,0 +1,15 @@
+const sides = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#hypotenuse-btn");
+const output = document.querySelector("#output");
+
+function calculateSumOfSquares(a, b){
+    const sumofSquares = a*a + b*b;
+    return sumofSquares;
+}
+
+function calculateHypotenuse(){
+    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
+    output.innerText = "The Hypotenuse is " + Math.sqrt(sumOfSquares);
+}
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);
--- quiz.js
+++ quiz.js
@@ -4,3 +4,17 @@ const outputEl = document.querySelector("#output");
 
 const correctAnswers = ["90°", "right angled"];
 
+function calculateScore(){
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === correctAnswers[index]){
+            score++;
+        }
+        index++;
+    }
+    outputEl.innerText = "Your score is " + score;
+}
+
+submitAnswerBtn.addEventListener("click", calculateScore);
