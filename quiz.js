64a2b726f7b74effa13704a99e258c9f04962040
akashvinchankar <86399324+akashvinchankar@users.noreply.github.com>
2021-12-09 21:02:03 +0530

    2 questions added

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form");
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 const outputEl = document.querySelector("#output");
 
-const correctAnswers = ["90°", "right angled"];
+const correctAnswers = ["90°", "right angled", "A triangle with three angles of 60 degrees is equilateral", "All sides are equal"];
 
 function calculateScore(){
     let score = 0;
